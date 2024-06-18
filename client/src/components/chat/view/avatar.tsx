baf86a34f0ba2f039daf2be35b85af4c69002bee
jfilby <jason.filby@gmail.com>
2024-06-18 13:34:04 +0200

    Fix case statements for chat Avatar.

--- client/src/components/chat/view/avatar.tsx
+++ client/src/components/chat/view/avatar.tsx
@@ -13,11 +13,14 @@ export default function Message({ from }: Props) {
     // Render switch in a function: https://stackoverflow.com/a/52618847
     switch (from) {
 
-      case undefined || `User`:
+      case undefined:
+      case `User`:
         return <Avatar sx={{ bgcolor: deepPurple[500] }}>You</Avatar>
 
       default:
-        return <Avatar sx={{ bgcolor: deepOrange[500] }}>{from.substring(0, 2).toUpperCase()}</Avatar>
+        return <Avatar sx={{ bgcolor: deepOrange[500] }}>
+            {from.substring(0, 2).toUpperCase()}
+          </Avatar>
     }
   }
 
