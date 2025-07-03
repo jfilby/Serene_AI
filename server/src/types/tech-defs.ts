d584ee581977ec65cb0d42a21c2775283089bb30
jfilby <jason.filby@gmail.com>
2025-07-03 15:16:44 +0200

    Update TechDefs class with latest select Gemini and OpenAI models.

--- server/src/types/tech-defs.ts
+++ server/src/types/tech-defs.ts
@@ -28,71 +28,96 @@ export class AiTechDefs {
   static mockedLlmPaid = 'Mocked LLM'
   static mockedLlmFree = 'Mocked LLM (free tier)'
 
-  // Last updated: 18th Feb 2024
+  // Last updated: 3rd July Feb 2025
   // Run the Setup in /admin/setup to install new variants and effect any
   // upgrade paths in service/tech/data/llms.ts.
-  static googleGeminiV1Pro = 'Google Gemini v1 Pro'
-  static googleGeminiV1pt5Pro = 'Google Gemini v1.5 Pro'
-  static googleGeminiV1pt5Flash = 'Google Gemini v1.5 Flash'
-  static googleGeminiV2Flash = 'Google Gemini v2 Flash'
-  static googleGeminiV2FlashFree = 'Google Gemini v2 Flash (free tier)'
-  static googleGeminiLatestExpFree = 'Google Gemini Latest Experimental (free tier)'
-
-  static genericModel = 'Generic'
-  static v1ProVersion = 'v1 Pro'
-  static v1pt5ProVersion = 'v1.5 Pro'
-  static v1pt5FlashVersion = 'v1.5 Flash'
-  static v2FlashVersion = 'v2 Flash'
-  static expVersion = 'Experimental'
+  // static googleGemini_V1Pro = 'Google Gemini v1 Pro'
+  // static googleGemini_V1pt5Pro = 'Google Gemini v1.5 Pro'
+  // static googleGemini_V1pt5Flash = 'Google Gemini v1.5 Flash'
+  static googleGemini_V2Flash = 'Google Gemini v2 Flash'
+  static googleGemini_V2FlashFree = 'Google Gemini v2 Flash (free tier)'
+  static googleGemini_LatestExpFree = 'Google Gemini Latest Experimental (free tier)'
+  static googleGemini_V2pt5Pro = 'Google Gemini v2.5 Pro'
+  static googleGemini_V2pt5Flash = 'Google Gemini v2.5 Flash'
+  static googleGemini_V2pt5FlashLite = 'Google Gemini v2.5 Flash-Lite'
 
   // OpenAI
-  static chatGpt4o = 'GPT-4o'
+  static openAi_Gpt4o = 'GPT-4o'
+  static openAi_Gpt4pt1 = 'GPT-4.1'
+  static openAi_O4Mini = 'o4-mini'
+  static openAi_O3 = 'o3'
 
   // OpenRouter
   static deepSeekR1_0528_Chutes = 'DeepSeek R1 (0528)'
 
-  // Project default
-  static defaultLlmVariantName = this.googleGeminiV1pt5Pro
-  static defaultChatVariantName = this.chatGpt4o
-
   // A list of available LLMs
   static userAILlms = [
     // Google Gemini
-    {
+    /* {
       provider: this.googleGeminiProvider,
-      variantName: this.googleGeminiV1Pro,
+      variantName: this.googleGemini_V1Pro,
       default: false
     },
     {
       provider: this.googleGeminiProvider,
-      variantName: this.googleGeminiV1pt5Pro,
+      variantName: this.googleGemini_V1pt5Pro,
       default: false
     },
     {
       provider: this.googleGeminiProvider,
-      variantName: this.googleGeminiV1pt5Flash,
+      variantName: this.googleGemini_V1pt5Flash,
+      default: false
+    }, */
+    {
+      provider: this.googleGeminiProvider,
+      variantName: this.googleGemini_V2Flash,
       default: false
     },
     {
       provider: this.googleGeminiProvider,
-      variantName: this.googleGeminiV2Flash,
+      variantName: this.googleGemini_V2FlashFree,
       default: false
     },
     {
       provider: this.googleGeminiProvider,
-      variantName: this.googleGeminiV2FlashFree,
-      default: false
-    },
-    {
-      provider: this.googleGeminiProvider,
-      variantName: this.googleGeminiLatestExpFree,
+      variantName: this.googleGemini_LatestExpFree,
       default: true
+    },
+    {
+      provider: this.googleGeminiProvider,
+      variantName: this.googleGemini_V2pt5Pro,
+      default: false
+    },
+    {
+      provider: this.googleGeminiProvider,
+      variantName: this.googleGemini_V2pt5Flash,
+      default: false
+    },
+    {
+      provider: this.googleGeminiProvider,
+      variantName: this.googleGemini_V2pt5FlashLite,
+      default: false
     },
 
     // OpenAI
     {
       provider: this.chatGptProvider,
-      variantName: this.chatGpt4o,
+      variantName: this.openAi_Gpt4o,
+      default: false
+    },
+    {
+      provider: this.chatGptProvider,
+      variantName: this.openAi_Gpt4pt1,
+      default: false
+    },
+    {
+      provider: this.chatGptProvider,
+      variantName: this.openAi_O4Mini,
+      default: false
+    },
+    {
+      provider: this.chatGptProvider,
+      variantName: this.openAi_O3,
       default: false
     },
     // OpenRouter
@@ -120,64 +145,38 @@ export class AiTechDefs {
   static googleGeminiV1pt5Flash_ModelName = 'gemini-1.5-flash'
   static googleGeminiV2Flash_ModelName = 'gemini-2.0-flash'
   static googleGeminiLatestExp_ModelName = 'gemini-2.0-pro-exp-02-05'
+  static googleGeminiV2pt5Pro_ModelName = 'gemini-2.5-pro'
+  static googleGeminiV2pt5Flash_ModelName = 'gemini-2.5-flash'
+  static googleGeminiV2pt5FlashLite_ModelName = 'gemini-2.5-flash-lite-preview-06-17'
 
-  static chatGpt4o_ModelName = 'gpt-4o'
+  static openAiGpt4o_ModelName = 'gpt-4o'
+  static openAiGpt4pt1_ModelName = 'gpt-4.1-2025-04-14'
+  static openAiO4Mini_ModelName = 'o4-mini-2025-04-16'
+  static openAiO3_ModelName = 'o3-2025-04-16'
 
   static openRouterDeepSeekR1_0528_Chutes_ModelName = 'deepseek/deepseek-r1-0528:free'
 
   // Variant to model names
   static variantToModelNames = {
     // Google Gemini
-    [AiTechDefs.googleGeminiV1Pro]: this.googleGeminiV1Pro_ModelName,
+    /* [AiTechDefs.googleGeminiV1Pro]: this.googleGeminiV1Pro_ModelName,
     [AiTechDefs.googleGeminiV1pt5Pro]: this.googleGeminiV1pt5Pro_ModelName,
-    [AiTechDefs.googleGeminiV1pt5Flash]: this.googleGeminiV1pt5Flash_ModelName,
-    [AiTechDefs.googleGeminiV2Flash]: this.googleGeminiV2Flash_ModelName,
-    [AiTechDefs.googleGeminiV2FlashFree]: this.googleGeminiV2Flash_ModelName,
-    [AiTechDefs.googleGeminiLatestExpFree]: this.googleGeminiLatestExp_ModelName,
+    [AiTechDefs.googleGeminiV1pt5Flash]: this.googleGeminiV1pt5Flash_ModelName, */
+    [AiTechDefs.googleGemini_V2Flash]: this.googleGeminiV2Flash_ModelName,
+    [AiTechDefs.googleGemini_V2FlashFree]: this.googleGeminiV2Flash_ModelName,
+    [AiTechDefs.googleGemini_LatestExpFree]: this.googleGeminiLatestExp_ModelName,
+    [AiTechDefs.googleGemini_V2pt5Pro]: this.googleGeminiV2pt5Pro_ModelName,
+    [AiTechDefs.googleGemini_V2pt5Flash]: this.googleGeminiV2pt5Flash_ModelName,
+    [AiTechDefs.googleGemini_V2pt5FlashLite]: this.googleGeminiV2pt5FlashLite_ModelName,
 
     // OpenAI
-    [AiTechDefs.chatGpt4o]: this.chatGpt4o_ModelName,
+    [AiTechDefs.openAi_Gpt4o]: this.openAiGpt4o_ModelName,
+    [AiTechDefs.openAi_Gpt4pt1]: this.openAiGpt4pt1_ModelName,
+    [AiTechDefs.openAi_O4Mini]: this.openAiO4Mini_ModelName,
+    [AiTechDefs.openAi_O3]: this.openAiO3_ModelName,
 
     // OpenRouter
     [AiTechDefs.deepSeekR1_0528_Chutes]: this.openRouterDeepSeekR1_0528_Chutes_ModelName
-  }
-
-  // Variant to providers
-  static variantToProviders = {
-    [AiTechDefs.mockedLlmPaid]: this.mockedProvider,
-    [AiTechDefs.mockedLlmFree]: this.mockedProvider,
-
-    [AiTechDefs.googleGeminiV1Pro]: this.googleGeminiProvider,
-    [AiTechDefs.googleGeminiV1pt5Pro]: this.googleGeminiProvider,
-    [AiTechDefs.googleGeminiV1pt5Flash]: this.googleGeminiProvider,
-    [AiTechDefs.googleGeminiV2Flash]: this.googleGeminiProvider,
-    [AiTechDefs.googleGeminiV2FlashFree]: this.googleGeminiProvider,
-    [AiTechDefs.googleGeminiLatestExpFree]: this.googleGeminiProvider,
-
-    [AiTechDefs.chatGpt4o]: this.chatGptProvider,
-    [AiTechDefs.deepSeekR1_0528_Chutes]: this.openRouterProvider
-  }
-
-  // Variant names to descriptions
-  static variantNamesToDescriptions = {
-
-    // Mocked
-    [AiTechDefs.mockedLlmPaid]: 'Mocked LLM',
-    [AiTechDefs.mockedLlmFree]: 'Mocked LLM (free iter)',
-
-    // Google Gemini
-    [AiTechDefs.googleGeminiV1Pro]: 'Gemini v1 Pro',
-    [AiTechDefs.googleGeminiV1pt5Pro]: 'Gemini v1.5 Pro',
-    [AiTechDefs.googleGeminiV1pt5Flash]: 'Gemini v1.5 Flash',
-    [AiTechDefs.googleGeminiV2Flash]: 'Gemini v2 Flash',
-    [AiTechDefs.googleGeminiV2FlashFree]: 'Gemini v2 Flash (free)',
-    [AiTechDefs.googleGeminiLatestExpFree]: 'Gemini Latest Experimental (free)',
-
-    // OpenAI
-    [AiTechDefs.chatGpt4o]: 'GPT-4o',
-
-    // OpenRouter
-    [AiTechDefs.deepSeekR1_0528_Chutes]: 'DeepSeek R1 (0528) (OpenRouter/Chutes)'
   }
 
   // Define a large context token size
@@ -195,15 +194,21 @@ export class AiTechDefs {
 
     // Google Gemini
     // Source: https://ai.google.dev/models/gemini
-    [AiTechDefs.googleGeminiV1Pro]: 1048576,
-    [AiTechDefs.googleGeminiV1pt5Pro]: 2097152,
-    [AiTechDefs.googleGeminiV1pt5Flash]: 1048576,
-    [AiTechDefs.googleGeminiV2Flash]: 1048576,
-    [AiTechDefs.googleGeminiV2FlashFree]: 1048576,
-    [AiTechDefs.googleGeminiLatestExpFree]: 1048576,
+    /* [AiTechDefs.googleGemini_V1Pro]: 1048576,
+    [AiTechDefs.googleGemini_V1pt5Pro]: 2097152,
+    [AiTechDefs.googleGemini_V1pt5Flash]: 1048576, */
+    [AiTechDefs.googleGemini_V2Flash]: 1048576,
+    [AiTechDefs.googleGemini_V2FlashFree]: 1048576,
+    [AiTechDefs.googleGemini_LatestExpFree]: 1048576,
+    [AiTechDefs.googleGemini_V2pt5Pro]: 1048576,
+    [AiTechDefs.googleGemini_V2pt5Flash]: 1048576,
+    [AiTechDefs.googleGemini_V2pt5FlashLite]: 1000000,
 
     // OpenAI
-    [AiTechDefs.chatGpt4o]: 128000,
+    [AiTechDefs.openAi_Gpt4o]: 128000,
+    [AiTechDefs.openAi_Gpt4pt1]: 1047576,
+    [AiTechDefs.openAi_O4Mini]: 200000,
+    [AiTechDefs.openAi_O3]: 200000,
 
     // OpenRouter
     [AiTechDefs.deepSeekR1_0528_Chutes]: 163840
@@ -217,15 +222,21 @@ export class AiTechDefs {
 
     // Google Gemini
     // Source: https://ai.google.dev/models/gemini
-    [AiTechDefs.googleGeminiV1Pro]: 8192,
-    [AiTechDefs.googleGeminiV1pt5Pro]: 8192,
-    [AiTechDefs.googleGeminiV1pt5Flash]: 8192,
-    [AiTechDefs.googleGeminiV2Flash]: 8192,
-    [AiTechDefs.googleGeminiV2FlashFree]: 8192,
-    [AiTechDefs.googleGeminiLatestExpFree]: 8192,
+    /* [AiTechDefs.googleGemini_V1Pro]: 8192,
+    [AiTechDefs.googleGemini_V1pt5Pro]: 8192,
+    [AiTechDefs.googleGemini_V1pt5Flash]: 8192, */
+    [AiTechDefs.googleGemini_V2Flash]: 8192,
+    [AiTechDefs.googleGemini_V2FlashFree]: 8192,
+    [AiTechDefs.googleGemini_LatestExpFree]: 8192,
+    [AiTechDefs.googleGemini_V2pt5Pro]: 65536,
+    [AiTechDefs.googleGemini_V2pt5Flash]: 65536,
+    [AiTechDefs.googleGemini_V2pt5FlashLite]: 64000,
 
     // OpenAI
-    [AiTechDefs.chatGpt4o]: 16384,
+    [AiTechDefs.openAi_Gpt4o]: 16384,
+    [AiTechDefs.openAi_Gpt4pt1]: 32768,
+    [AiTechDefs.openAi_O4Mini]: 100000,
+    [AiTechDefs.openAi_O3]: 100000,
 
     // OpenRouter
     [AiTechDefs.deepSeekR1_0528_Chutes]: 163840
